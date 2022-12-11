0eceb64e7ab64cdfb31b952c41320f250e6d6b9b
viniciuspch <vinicius.geral2018@gmail.com>
2022-12-11 16:54:35 -0300

    Main structure of the program completed

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -1,9 +1,58 @@
 package Program;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		List<Product> list = new ArrayList<>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+		System.out.println("Enter the number of products: ");
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			System.out.println("Product" + " #" + (i + 1) + " data: ");
+			System.out.println("Common, used or imported? (c/u/i)");
+			char option = sc.next().charAt(0);
+			System.out.println("Name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.println("Price: ");
+			Double productPrice = sc.nextDouble();
+			System.out.println();
+			if (option == 'c') {
+
+				list.add(new Product(productName, productPrice));
+
+			} else if (option == 'u') {
+				System.out.println("Manufacture date (DD/MM/YYYY): ");
+				Date manufactureDate = sdf.parse(sc.next());
+				list.add(new UsedProduct(productName, productPrice, manufactureDate));
+			} else if (option == 'i') {
+				System.out.println("Customs fee: ");
+				Double customsFee = sc.nextDouble();
+				list.add(new ImportedProduct(productName, productPrice, customsFee));
+			} else {
+				System.out.println("Invalid option!");
+			}
+		}
+		System.out.println("");
+		System.out.println("PRICE TAGS: ");
+		for (Product prod : list) {
+			System.out.println(prod.priceTag());
+		}
+
+		sc.close();
 	}
 
 }
